85bbf693161d0bc5d5ae6531cd19129a29d3aadb
Maksim <maksimus-94@tut.by>
2021-11-15 10:34:38 +0300

    SignUp and 404Page

--- social-network/src/components/notfound-page/NotFoundPage.js
+++ social-network/src/components/notfound-page/NotFoundPage.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {Button, Col, Container, Row} from "react-bootstrap";
+
 
 const NotFoundPage = () => {
     return (
-        <div>
-            NotFoundPage
-        </div>
+
+        <>
+            <Container className={'mt-5'}>
+                <Row>
+                    <Col className={'text-center'}>
+                        <h2>404 Error Page</h2>
+                    </Col>
+                </Row>
+                <Row className={'text-center'}>
+                    <Col>
+                        <p className={'text-danger fs-5'}>Page not found</p>
+                    </Col>
+                </Row>
+                <div className={'d-grid mt-4'}>
+                    <Button variant="outline-dark" size="lg">
+                        Go Back to the Main Page
+                    </Button>
+                </div>
+            </Container>
+        </>
     );
 };
 
--- social-network/src/components/signup/SignUp.js
+++ social-network/src/components/signup/SignUp.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import {Button, Col, Container, Form, Row} from "react-bootstrap";
 
 const SignUp = () => {
     return (
-        <div>
-            SignUp
-        </div>
+        <Container className={'mt-5'}>
+            <Form>
+                <div className={'text-center'}>
+                    <h2>SIGN UP</h2>
+                </div>
+                <Row className={'mt-2'}>
+                    <Col xs={12} md={6} className={'mt-2'}>
+                        <Form.Control placeholder="First name"/>
+                    </Col>
+                    <Col xs={12} md={6} className={'mt-2'}>
+                        <Form.Control placeholder="Last name"/>
+                    </Col>
+                </Row>
+                <Row className={'mt-2'}>
+                    <Col>
+                        <Form.Control type={'email'} placeholder="Email"/>
+                    </Col>
+                </Row>
+                <Row className={'mt-2'}>
+                    <Col xs={12} md={6} className={'mt-2'}>
+                        <Form.Control type={'password'} placeholder="Password"/>
+                    </Col>
+                    <Col xs={12} md={6} className={'mt-2'}>
+                        <Form.Control type={'password'} placeholder="Confirm Password"/>
+                    </Col>
+                </Row>
+                <div className={'d-grid mt-4'}>
+                    <Button variant="outline-info" size="lg">
+                        Sign UP
+                    </Button>
+                </div>
+            </Form>
+        </Container>
     );
 };
 
