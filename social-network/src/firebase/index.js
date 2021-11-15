c31a181fdc1fd8c47ba75cee09459301a31238f9
Maksim <maksimus-94@tut.by>
2021-11-15 13:46:21 +0300

    Add Firebase

---
+++ social-network/src/firebase/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAqYMIyyOcS7jvjV7IA4GFyiECl7gH7PrE",
+    authDomain: "react-social-network-acf47.firebaseapp.com",
+    databaseURL: "https://react-social-network-acf47-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "react-social-network-acf47",
+    storageBucket: "react-social-network-acf47.appspot.com",
+    messagingSenderId: "40125799895",
+    appId: "1:40125799895:web:b7065a58204e2a2c56bf92"
+};
+
+const app = initializeApp(firebaseConfig);
